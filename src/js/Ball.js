bcbee11bd04e27cedc71421f1ae2802ff48c3e15
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2020-10-21 01:15:57 +0100

    Creates realistic wall collision

--- src/js/Ball.js
+++ src/js/Ball.js
@@ -1,49 +1,36 @@
-class Ball extends Component{
-    constructor(x, y, z,raio,material) {
-        super(x, y, z);
-        this.ball_mesh = this.addSphere(material,0,0,0,raio);
+class Ball extends Component {
+    constructor(x, y, z,radius,material) {
+        super();
+        this.ballMesh = this.addSphere(material, 0, 0, 0, raio);
         this.position.x = x;
         this.position.y = y;
         this.position.z = z;
         this.mass = 1;
         this.axis = new THREE.AxesHelper(3);
         this.hasAxis = false;
-        this.velocity ={
-            x: Math.random() -0.7 ,
-            y: Math.random() -0.7
+        this.velocity = {
+            x: Math.random() - 0.5,
+            y: Math.random() - 0.5
         };
+        this.radius = radius;
 
-        this.radius = raio
+/*
+        this.update = balls => {
 
-
-        /*this.update = balls => {
-            
-            for(let i =0;i<balls.length;i++)
-            {
-                     if(this===balls[i]) continue;
-                     if(distance(this.position.x,this.position.y,balls[i].position.x,
-                        balls[i].position.y) - this.radius*2<0)
-                        {
-                            /*Ha colisao!!*/
-                    /*        console.log("colision");
-                        }	
+            for (let i = 0; i < balls.length; i++) {
+                if (this === balls[i]) continue;
+                if (distance(this.position.x, this.position.y, balls[i].position.x,
+                    balls[i].position.y) - this.radius * 2 < 0) {
+                    // Ha colisao!!
+                    console.log("colision");
                 }
-                if(this.x-this.radius < )
+            }
+            if (this.x - this.radius <)
                 this.position.x += this.velocity.x;
-                this.position.y += this.velocity.y;
-            }*/
-            
-        
-        
-
+            this.position.y += this.velocity.y;
         }
-        
-        
-        
-        
-       
-        
-
+*/
+    }
     addBallAxis() {
         this.hasAxis = !this.hasAxis;
         if (this.hasAxis)
@@ -51,15 +38,4 @@ class Ball extends Component{
         else
             this.remove(this.axis);
     }
-
-
-
-
-
-
-
-
-
-
-
 }
