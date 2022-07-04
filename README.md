ab0728f490cf1918b888e91d1c641a04ac0b86bf
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-07-04 18:03:29 +0100

    Convert to npm package

---
+++ README.md
@@ -0,0 +1,7 @@
+# threejs-camera-and-collisions
+
+A project developed around the Three.js 3D library, exploring the concepts moving cameras and collisions.
+
+## Background
+
+This project was made for the Computer Graphics course's 2020/21 edition at Instituto Superior Técnico, as part of the degree in Computer Science and Engineering. Any improvements after Oct 26, 2020 was merely out of pure curiosity.
---
+++ src/Ball.js
@@ -0,0 +1,246 @@
+import * as THREE from "three";
+import Component from "./Component";
+
+export default class Ball extends Component {
+  constructor(radius, poolTable, x, y, material, velocity) {
+    super();
+    this.radius = radius;
+    this.position.set(x, y, this.radius);
+    this.velocity = velocity;
+
+    this.mass = 1;
+    this.poolTable = poolTable;
+    this.addSphere(material, 0, 0, 0, this.radius);
+
+    this.hasAxis = false;
+    this.axis = new THREE.AxesHelper(3);
+    this.isFalling = false;
+  }
+
+  // Adds an axis to the ball
+  addBallAxis() {
+    this.hasAxis = !this.hasAxis;
+    if (this.hasAxis) {
+      this.add(this.axis);
+    } else {
+      this.remove(this.axis);
+    }
+  }
+
+  // Gets distance between two balls
+  distanceToBall(ball_x, ball_y) {
+    let xDistance = ball_x - this.position.x;
+    let yDistance = ball_y - this.position.y;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  }
+
+  // Moves the ball according to its velocity and the time since it was last called
+  move(timeDelta) {
+    // Adds friction and slows the movement
+    this.velocity.x *= this.poolTable.drag;
+    this.velocity.y *= this.poolTable.drag;
+
+    let velocityX = this.velocity.x * timeDelta;
+    let velocityY = this.velocity.y * timeDelta;
+    let velocityZ = this.velocity.z * timeDelta;
+
+    if (!this.isFalling) {
+      this.poolTable.holes.forEach((hole) => {
+        // Each hole has 4 equidistant points from the path used to create the hole
+        // The first point has its y at the center and the second its x
+        let centerX = hole[1].x;
+        let centerY = hole[0].y;
+
+        // Checks if ball has entered the hole
+        if (
+          Math.abs(this.position.x - centerX) < this.radius * 1.5 &&
+          Math.abs(this.position.y - centerY) < this.radius * 1.5
+        ) {
+          this.velocity.z -= 10;
+
+          this.isFalling = true;
+          return;
+        }
+      });
+      let hasCollided = false;
+
+      let distance =
+        this.poolTable.innerDepth / 2 -
+        (Math.abs(this.position.x) + this.radius);
+
+      //checks if the ball has colided the wall
+      if (
+        distance <= Math.abs(velocityX) &&
+        Math.sign(this.position.x) === Math.sign(velocityX)
+      ) {
+        hasCollided = true;
+        // TODO: #2 Second condition can't be the way it is: if this.velocity.x > poolTable.innerDepth / 2, it fails
+
+        // When the velocity of the ball is higher than the distance to the wall
+        // The ball will be positionated at position where it should be if the ball
+        // hit the ball and returned back
+        this.position.x =
+          Math.sign(this.position.x) *
+          (this.poolTable.innerDepth / 2 -
+            this.radius -
+            (velocityX - distance) * this.poolTable.wallCOR);
+
+        this.rotateY((2 * distance - velocityX) / this.radius);
+        this.velocity.x = -this.velocity.x * this.poolTable.wallCOR;
+      }
+
+      distance =
+        this.poolTable.innerWidth / 2 -
+        (Math.abs(this.position.y) + this.radius);
+      // Checks if the ball has colided the wall
+      if (
+        distance <= Math.abs(velocityY) &&
+        Math.sign(this.position.y) === Math.sign(velocityY)
+      ) {
+        // TODO: #1 Second condition can't be the way it is: if this.velocity.y > poolTable.innerWidth / 2, it fails
+        hasCollided = true;
+        this.position.y =
+          Math.sign(this.position.y) *
+          (this.poolTable.innerWidth / 2 -
+            this.radius -
+            (velocityY - distance) * this.poolTable.wallCOR);
+        this.rotateX(-(2 * distance - velocityY) / this.radius);
+        this.velocity.y = -this.velocity.y * this.poolTable.wallCOR;
+      }
+
+      if (hasCollided) {
+        return;
+      }
+    } else {
+      // Ball fell in a hole
+      this.position.z += velocityZ;
+      this.velocity.z -= 10;
+    }
+    // Update position of balls
+    this.position.x += velocityX;
+    this.rotateY(velocityX / this.radius);
+    this.position.y += velocityY;
+    this.rotateX(-velocityY / this.radius);
+  }
+
+  rotate(velocity, angle) {
+    return {
+      x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+      y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
+    };
+  }
+
+  dot_product(ball, other) {
+    // The other_ball is the referential
+
+    // Velocity vector from ball (direction that the ball is moving),
+    // seen from other_ball referential
+    let xVelocityDelta = ball.velocity.x - other.velocity.x;
+    let yVelocityDelta = ball.velocity.y - other.velocity.y;
+
+    // Direction vector from other_ball to ball
+    let xDistance = other.position.x - ball.position.x;
+    let yDistance = other.position.y - ball.position.y;
+
+    // If Distance vector points in the same direction as the Velocity vector,
+    // this value will be positive and we will have to resolve the collision
+    return xVelocityDelta * xDistance + yVelocityDelta * yDistance;
+  }
+
+  resolveCollision(ball, other) {
+    if (ball.position.z !== this.position.z) {
+      return;
+    }
+
+    let xDistance = other.position.x - ball.position.x;
+    let yDistance = other.position.y - ball.position.y;
+
+    let dot_product = this.dot_product(ball, other);
+
+    // The dot product will be positive if the angle between both vectors is smaller than
+    // 90 degrees (if velocity vector points somewhat in the same direction of distance vector)
+    // and negative otherwise.
+
+    // Resolve collision if the balls are moving towards each other
+    // See the end of the page for further explanation
+
+    if (dot_product >= 0) {
+      // Angle between two balls
+      let angle = -Math.atan2(yDistance, xDistance);
+
+      let mBall = ball.mass;
+      let mOther = other.mass;
+
+      //Rotates the velocity vectors to compute in 1D
+      let initialVelocity_Ball = this.rotate(ball.velocity, angle);
+      let initialVelocity_Other = this.rotate(other.velocity, angle);
+
+      //Elastic colision:
+      //v1 = [(m1-m2)/(m1+m2)] * v0(1) + 2 * [m2 / (m1+m2)] * v0(2)
+      //v2 = [(m2-m1)/(m1+m2)] * v0(2) + 2 * [m1 / (m1+m2)] * v0(1)
+
+      //Final velocity in 1D - Ball
+      let finalVelocity_Ball = {
+        x:
+          (initialVelocity_Ball.x * (mBall - mOther)) / (mBall + mOther) +
+          (initialVelocity_Other.x * 2 * mOther) / (mBall + mOther),
+
+        y: initialVelocity_Ball.y,
+      };
+
+      //Final velocity in 1D - Other
+      let finalVelocity_Other = {
+        x:
+          (initialVelocity_Other.x * (mBall - mOther)) / (mBall + mOther) +
+          (initialVelocity_Ball.x * 2 * mBall) / (mBall + mOther), //fixed here m1, instead of m2
+        y: initialVelocity_Other.y,
+      };
+
+      //Final velocity in 2D
+      finalVelocity_Ball = this.rotate(finalVelocity_Ball, -angle);
+      finalVelocity_Other = this.rotate(finalVelocity_Other, -angle);
+
+      // Set final result
+      ball.velocity.x = finalVelocity_Ball.x;
+      ball.velocity.y = finalVelocity_Ball.y;
+
+      other.velocity.x = finalVelocity_Other.x;
+      other.velocity.y = finalVelocity_Other.y;
+    }
+  }
+
+  update(timeDelta, balls, size) {
+    this.move(timeDelta);
+
+    for (let i = 0; i < size; i++) {
+      if (this == balls[i]) {
+        continue;
+      }
+
+      if (
+        this.distanceToBall(balls[i].position.x, balls[i].position.y) <=
+        this.radius * 2
+      ) {
+        // The two balls have colided
+        this.resolveCollision(this, balls[i]);
+
+        let velocityX = this.velocity.x * timeDelta;
+        let velocityY = this.velocity.y * timeDelta;
+        this.position.x += velocityX;
+        this.rotateY(velocityX / this.radius);
+        this.position.y += velocityY;
+        this.rotateX(-velocityY / this.radius);
+      }
+    }
+  }
+}
+
+// Collisions:
+
+// If the balls are colliding but not moving towards each other, the collision will resolve itself.
+// If we do something in that case, the collision will not resolve itself and we will make it worst
+// (we keep changing v1 and v2 - that's what makes the balls rotate with one another).
+
+// This is only a problem when we are in a 2D space, because, in 1D space, if the balls collide with
+// each other, the balls are necessarily moving towards each other.
